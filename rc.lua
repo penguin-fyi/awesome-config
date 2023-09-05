5b82ab597a99cb0ac6b2aae8d0128d33a461f543
Jeff M. Hubbard <jeffmhubbard@gmail.com>
2023-09-05 02:55:51 -0500

    move vars to rc.lua

--- rc.lua
+++ rc.lua
@@ -13,42 +13,54 @@ require 'utils.error_handling'()
 
 -- Import XSETTINGS before theme
 require 'utils.xsettings'({
-  path                = cfg.paths.xsd_conf
+  path = os.getenv('HOME')..'/.config/xsettingsd/xsettingsd.conf'
 })
 
 -- Load theme
-local theme             = require 'themes.ngui.theme'
+local theme = require 'themes.ngui.theme'
 beautiful.init(theme)
 
 -- Connect signals
 require 'signals.tag.layouts'({
-  layout_list         = cfg.vars.tag_default_layouts,
+  layout_list = {
+    awful.layout.suit.floating,
+    awful.layout.suit.tile.left,
+    awful.layout.suit.fair,
+    awful.layout.suit.fair.horizontal,
+    awful.layout.suit.tile,
+  }
 })
 
 require 'signals.screen.tags'({
-  tags_list           = cfg.vars.screen_tags_list,
-  tags_auto           = cfg.vars.screen_tags_auto,
+  tags_auto = 4,
 })
 require 'signals.screen.wibar'({
-  taglist_tooltip     = cfg.vars.wibar_taglist_tooltip,
-  tasklist_tooltip    = cfg.vars.wibar_tasklist_tooltip,
+  calendar_enabled = true,
+  taglist_tooltip = true,
+  tasklist_tooltip = true,
 })
 require 'signals.screen.session'()
 require 'signals.screen.wallpaper'()
 
 require 'signals.client.titlebars' ({
-    tooltips            = cfg.vars.titlebar_enable_tooltip,
-    fade                = cfg.vars.titlebar_unfocus_fade,
+  tooltips = true,
+  fade = 0.25,
 })
 require 'signals.client.placement'()
-require 'signals.client.focus'()
+require 'signals.client.focus'({
+  sloppy = true,
+})
 require 'signals.client.icon'()
 require 'signals.client.bindings'()
 
 
 require 'signals.naughty.display'()
 require 'signals.naughty.icons'({
-    icon_dirs           = cfg.paths.icon_search_dirs,
+  icon_dirs = {
+    '/usr/share/icons/Papirus/',
+    '/usr/share/icons/gnome/',
+    '/usr/share/pixmaps/',
+  }
 })
 
 -- Global bindings
@@ -61,5 +73,8 @@ require 'rules'
 
 -- XDG autostart
 require 'utils.autostart'({
-  dirs                = cfg.paths.autostart_dirs,
+  dirs = {
+    '/etc/xdg/autostart/',
+    '$XDG_CONFIG_HOME/autostart/',
+  }
 })
